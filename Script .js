158051ebb2df244738f7b26fd05b4d1a9d00b212
Kevin Richard <kevinrichardve@gmail.com>
2022-01-15 14:13:01 -0300

    meu codigo

--- Script .js
+++ Script .js
@@ -1,5 +1,27 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('#botao');
 const nomeDoPersonagem  = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
+
+
+pergarPersonagem = () => {
+    return fetch (`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET', 
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'aplication/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+
+    
+
+    });
+}
+
+botao.onclick = pergarPersonagem;
